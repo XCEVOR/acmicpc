fc84d83393ae4e95ce0d2655cf4bb68b50fa86f0
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2022-08-22 15:40:23 +0900

    Add files via upload

---
+++ 01436 - film director.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+series = 666
+
+while N != 0 :
+    if '666' in str(series) :
+        N = N - 1
+        if N == 0 :
+            break
+    series += 1
+    
+print(series)
+
+
+
+# 500
+# 166699
