29c3e849ddf197425758b0de4d02ecd7b908a65f
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2022-09-08 13:54:49 +0900

    added comments

--- 01085 - escape from rectangle.py
+++ 01085 - escape from rectangle.py
@@ -1,3 +1,7 @@
+# 한수는 지금 (x, y)에 있다. 
+# 직사각형은 각 변이 좌표축에 평행하고, 왼쪽 아래 꼭짓점은 (0, 0), 오른쪽 위 꼭짓점은 (w, h)에 있다. 
+# 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+
 import sys
 
 x, y, w, h = map(int, sys.stdin.readline().split())
@@ -6,3 +10,8 @@ distance_diff_all = [x, y, w - x, h - y]
 
 print(min(distance_diff_all))
 
+
+
+# 653 375 1000 1000
+
+# 347
