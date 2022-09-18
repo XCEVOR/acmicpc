3ec228e01ab9f7d77daa588fa7e94e90ceebfe44
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2022-09-18 10:05:17 +0900

    Create 02164 - card2.py

---
+++ 02164 - card2.py
@@ -0,0 +1,59 @@
+# N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 
+# 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
+
+# 이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 
+# 우선, 제일 위에 있는 카드를 바닥에 버린다. 
+# 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+
+# 예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 
+# 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 
+# 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 
+# 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
+
+# N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
+
+import sys
+inputdata = sys.stdin.readline
+
+from collections import deque
+
+N = int(inputdata())
+dq = deque([i for i in range(1, N + 1)])
+dq.reverse()
+
+while (len(dq) > 1):
+    dq.pop()
+    print(f'dq_pop: {dq}')                  # temp
+    
+    flooring_num = dq.pop()
+    
+    dq.appendleft(flooring_num)
+    print(f'dq_appendleft: {dq}')           # temp
+    
+print(dq[0])
+
+
+
+# INPUT -> 2 OUTPUT -> 2
+# INPUT -> 3 OUTPUT -> 2
+# INPUT -> 4 OUTPUT -> 4
+# INPUT -> 5 OUTPUT -> 2
+# INPUT -> 6 OUTPUT -> 4
+# INPUT -> 7 OUTPUT -> 6
+# INPUT -> 8 OUTPUT -> 8
+
+
+
+# 6
+
+# dq_pop: deque([6, 5, 4, 3, 2])
+# dq_appendleft: deque([2, 6, 5, 4, 3])
+# dq_pop: deque([2, 6, 5, 4])
+# dq_appendleft: deque([4, 2, 6, 5])
+# dq_pop: deque([4, 2, 6])
+# dq_appendleft: deque([6, 4, 2])
+# dq_pop: deque([6, 4])
+# dq_appendleft: deque([4, 6])
+# dq_pop: deque([4])
+# dq_appendleft: deque([4])
+# 4
