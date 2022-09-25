d81e63e1cad8de9224168f27915de19d3e24bd47
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2022-09-25 18:17:05 +0900

    Create 02558 - A+B - 2.js

---
+++ 02558 - A+B - 2.js
@@ -0,0 +1,16 @@
+// # 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+const fs = require('fs');
+const inputdata = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const A = parseInt(inputdata[0]);
+const B = parseInt(inputdata[1]);
+
+console.log(A + B);
+
+
+
+// # 1
+// # 2
+
+// # 3
