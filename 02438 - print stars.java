a032178accde1c15bbc9360e26cc15c01a3c0daf
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-01-24 10:32:36 +0900

    Create 02438 - print stars.java

---
+++ 02438 - print stars.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static Scanner sc = new Scanner(System.in);
+		
+	static int numStar = sc.nextInt();
+
+
+	public static void main(String[] args) {
+		
+		int count = 0;
+		String star = "";
+		
+		fnRecursiveStar(count, star);
+		
+	}
+	
+	
+	public static void fnRecursiveStar(int count, String star) {
+	    
+	    if (numStar == count) {
+	        return;
+	    }
+	    
+	    star = star + "*";
+	    System.out.println(star);
+	    
+	    fnRecursiveStar(count + 1, star);
+	    
+	}
+
+
+}
+
