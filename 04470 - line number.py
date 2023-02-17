5cd69c303625207bc13a22690bc29e56602de1a9
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-02-17 23:43:01 +0900

    Create 04470 - line number.py

---
+++ 04470 - line number.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    n = int(input())
+    for i in range (n):
+        inputText = str(input())
+        result = str(i + 1) + ". " + inputText
+        
+        print(result)
