2c687467562479937ce5224500ca380c70dd140b
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-03-13 23:31:27 +0900

    Create 17863 - FYI.java

---
+++ 17863 - FYI.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        
+        String inputNumber = sc.next();
+        String subStrNumber = inputNumber.substring(0, 3);
+//        System.out.println(subStrNumber);  // TEXT PRINT.
+        
+        if (subStrNumber.equals("555")) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+
+    }
+}
