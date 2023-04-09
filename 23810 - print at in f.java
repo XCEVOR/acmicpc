026a922d61579e957c5243192c4d81467f1a7c07
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-04-09 23:42:27 +0900

    Create 23810 - print at in f.java

---
+++ 23810 - print at in f.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("@".repeat(5 * n));
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("@".repeat(n));
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("@".repeat(5 * n));
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("@".repeat(n));
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("@".repeat(n));
+        }
+
+    }
+}
