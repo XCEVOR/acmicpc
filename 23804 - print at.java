1b4670bf21123d93f66f2e2befe19512fefabf7d
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-04-26 23:48:41 +0900

    Create 23804 - print at.java

---
+++ 23804 - print at.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n * 5; i++) {
+            if (i <= n * 1) {
+                System.out.println("@".repeat(n * 5));
+            }
+            else if (i <= n * 4) {
+                System.out.println("@".repeat(n));
+            }
+            else {
+                System.out.println("@".repeat(n * 5));
+            }
+        }
+ 
+    }
+}
