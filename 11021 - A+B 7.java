398613487136c84de35e8c2a68f44a07c8dfdac4
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-06-18 21:09:58 +0900

    Create 11021 - A+B 7.java

---
+++ 11021 - A+B 7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int testCase = sc.nextInt();
+        
+        for (int i = 1; i <= testCase; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            int result = calcSum(A, B);
+            
+            System.out.println("Case #" + i + ": " + result);
+        }
+    }
+    
+    public static int calcSum(int A, int B) {
+        return A + B;
+    }
+    
+}
