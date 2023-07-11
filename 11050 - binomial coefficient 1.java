c79697804778169d1a0d6818934562cae02e98a3
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-07-11 23:41:54 +0900

    Create 11050 - binomial coefficient 1.java

---
+++ 11050 - binomial coefficient 1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        long result = factorial(n) / (factorial(k) * factorial(n - k));
+        
+        System.out.println(result);
+    }
+
+    private static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+    
+}
