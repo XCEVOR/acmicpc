f7dc7094385b62ef50b7c57d9aa38b52083959f5
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-07-27 23:23:11 +0900

    Create 16479 - cup height.java

---
+++ 16479 - cup height.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int K = sc.nextInt();
+        int D1 = sc.nextInt();
+        int D2 = sc.nextInt();
+        
+        double heightSquared = calculateCupHeightSquared(K, D1, D2);
+        
+        System.out.println(heightSquared);
+    }
+    
+    // 컵라면의 높이를 계산하는 함수
+    private static double calculateCupHeightSquared(int K, int D1, int D2) {
+        double r1 = D1 / 2.0;
+        double r2 = D2 / 2.0;
+        double height = Math.sqrt(K * K - Math.pow(r1 - r2, 2));
+        
+        return height * height;
+    }
+    
+}
