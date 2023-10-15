6afbf13da64935a0d85463702b306d9ea7618d71
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-10-15 22:34:37 +0900

    Create 10991 - print stars 16.java

---
+++ 10991 - print stars 16.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      
+        int n = sc.nextInt();
+      
+        for (int i = 1; i <= n; i++) {
+            // 공백 출력
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            // '* ' 출력
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            
+            // 줄 바꿈
+            System.out.println();
+        }
+    }
+    
+}
