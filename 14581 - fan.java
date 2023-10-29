d551c9114351fff45d4bebf54b84624b76d6ff7c
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2023-10-29 11:07:36 +0000

    Create 14581 - fan.java

---
+++ 14581 - fan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String name = sc.next();
+        
+        System.out.print(":fan::fan::fan:");
+        System.out.println();
+        
+        System.out.print(":fan:");
+        System.out.print(":");
+        System.out.print(name);
+        System.out.print(":");
+        System.out.print(":fan:");
+        System.out.println();
+        
+        System.out.print(":fan::fan::fan:");
+    }
+}
