c91d1715dc08f43388fa6eb34cbafe381d50df0a
HOCHAN <111336041+XCEVOR@users.noreply.github.com>
2024-03-05 23:51:13 +0900

    Create 26068 - coupon.java

---
+++ 26068 - coupon.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            String[] token = sc.next().split("-");
+            int remainingDays = Integer.parseInt(token[1]);
+            
+            if (remainingDays <= 90) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
