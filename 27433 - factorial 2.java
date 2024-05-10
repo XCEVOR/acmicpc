aee239d275dfa868e7d2c9bb7745278c2ccf4346
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2024-05-10 23:45:08 +0900

    Create 27433 - factorial 2.java

---
+++ 27433 - factorial 2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        long result = factorial(N);
+        
+        System.out.println(result);
+    }
+
+    private static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
