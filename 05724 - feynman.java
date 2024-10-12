277ec50ebae66257ef9658ba1888df85030807cf
XCEVOR <111336041+XCEVOR@users.noreply.github.com>
2024-10-12 21:35:48 +0900

    Create 05724 - feynman.java

---
+++ 05724 - feynman.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      int N = sc.nextInt();
+      
+      if (N == 0) break;
+      
+      System.out.println( countUniqueSquares(N) );
+    }
+  }
+  
+  private static int countUniqueSquares(int N) {
+    int totalSquares = 0;
+    
+    for (int k = 1; k <= N; k++) {
+      totalSquares += (N - k + 1) * (N - k + 1);
+    }
+    
+    return totalSquares;
+  }
+}
